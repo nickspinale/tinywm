62fb12af9a2e0a23c74f01a29e0e50995e12b0fc
Nick Spinale <spinalen@carleton.edu>
2016-08-31 10:45:58 +0200

    set crashonerror

--- Main.hs
+++ Main.hs
@@ -9,7 +9,7 @@ import Control.Monad
 import System.Exit
 
 main :: IO ()
-main = connect >>= maybe (die "failed to connect to x server") (unX (runMappingT tinywm) >=> either (die . show) return)
+main = connect >>= maybe (die "failed to connect to x server") (setCrashOnError >> (unX (runMappingT tinywm) >=> either (die . show) return))
 
 tinywm :: (MonadX x m, MappingCtx m) => m a
 tinywm = undefined
